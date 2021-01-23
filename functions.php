d6f30f04aa15ee7d2a3d254d208997e3a51c6252
Rajesh Mondal <rajesh01749@gmail.com>
2021-01-23 11:41:59 +0600

    child theme created

--- functions.php
+++ functions.php
@@ -1,1 +1,8 @@
 <?php
+
+function alpha_child_assets() {
+	wp_enqueue_style( "parent-style", get_parent_theme_file_uri( "/style.css" ) );
+	//wp_enqueue_style("alpha-style");
+}
+
+add_action( "wp_enqueue_scripts", "alpha_child_assets" );
